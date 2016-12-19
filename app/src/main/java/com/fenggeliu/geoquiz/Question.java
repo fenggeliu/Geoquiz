1bb2e8fb79e4be708c02ea091e71409199413bd6
Fengge <ryufengge@gmail.com>
2016-12-18 21:44:28 -0800

    Initial commit

---
+++ app/src/main/java/com/fenggeliu/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package com.fenggeliu.geoquiz;
+
+/**
+ * Created by fengge on 11/12/2016.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+}
